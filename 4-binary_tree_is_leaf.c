eedfeae4bf94215c60eb01f55326931d428bfb56
AbdullahSec <Talonkali@outlook.com>
2026-08-19 19:48:03 +0300

    finished 4-binary_tree_is_leaf.c

---
+++ 4-binary_tree_is_leaf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "binary_trees.h"
+/**
+ * binary_tree_is_leaf - check if the node has no child
+ * if has no child
+ * Return:1
+ *
+ */
+
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+
+if (node->left == NULL && node->right == NULL)
+return (1);
+else
+return (0);
+}
